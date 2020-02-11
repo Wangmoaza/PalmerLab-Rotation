69983365a017d8c1f340ec1241439a4c1c3f70a3
Wangmoaza <haeun0512@hanmail.net>
2020-02-11 15:31:45 -0500

    add adjust response functionality

--- predict_comb_therapy_effect.py
+++ predict_comb_therapy_effect.py
@@ -38,35 +38,71 @@ def parse_input(filepath):
     return ls
 
 
+def adjust_response(df, time, response):
+    """ Adjust response to make survival at {time} to {response} survival. """
+    # add pseudo point with survival (end time, 0) for scaling purposes
+    df.index = df.index + 1
+    df.loc[0, :] = [df.at[1, 'Time'], 0]
+    df = df.sort_index()
+
+    cutoff_idx = df[df['Time'] < time]['Survival'].idxmin()
+    # This does not make a copy. This operation will change the original data frame.
+    res = df.loc[:cutoff_idx, :].copy()
+    nonres = df.loc[cutoff_idx:, :].copy()
+    res.loc[:, 'Survival'] = np.interp(res['Survival'],
+                                       (res['Survival'].min(), res['Survival'].max()),
+                                       (0, response))
+    nonres.loc[:, 'Survival'] = np.interp(nonres['Survival'],
+                                          (nonres['Survival'].min(), nonres['Survival'].max()),
+                                          (response, 100))
+    # delete pseudo point
+    df = res.append(nonres)
+    df = df.drop(index=0)
+    df.index = df.index - 1
+    return df
+
+
 def main():
     parser = argparse.ArgumentParser(
         description="Predict combination effect of drugs",
         epilog='Implemented by Haeun Hwangbo')
     parser.add_argument(
-        'input', help='CSV File describing name and path of the survival data.\nThe ordering of the files should be drug A, B, A+B.\n(Column 1: name of drug, Column 2: path to survival data)')
+        'input', help='CSV File describing name and path of the survival data.\nThe ordering of the paths should be drug A, B, A+B.\n(Column 1: name of drug, Column 2: path to survival data)')
     parser.add_argument('--min-rho', type=float,
                         default=0.1, help='Minimum rho (default: 0.1)')
     parser.add_argument('--max-rho', type=float,
                         default=0.5, help='Maximum rho (default: 0.5)')
+    parser.add_argument('--adj-respA', nargs=2, type=float,
+                        help='Adjust resposne of treatment A to (survival) at (time).\nShould be in the format:\n--adj-respA time survival')
+    parser.add_argument('--adj-respB', nargs=2, type=float,
+                        help='Adjust resposne of treatment B to (survival) at (time).\nShould be in the format:\n--adj-respB time survival')
     parser.add_argument('--fig-height', type=int, default=5,
                         help='Output figure height (default: 5)')
     parser.add_argument('--fig-width', type=int, default=10,
                         help='Output figure width (default: 10)')
-    parser.add_argument('--out-prefix', help='Output file prefix. If not specified, it will be the names of the drugs.')
-    parser.add_argument('--extension', default = 'pdf', choices=['pdf', 'png', 'jpg'],
+    parser.add_argument(
+        '--out-prefix', help='Output file prefix. If not specified, it will be the names of the drugs.')
+    parser.add_argument('--extension', default='pdf', choices=['pdf', 'png', 'jpg'],
                         help='File extension for output figure')
     args = parser.parse_args()
 
+    # get input
     treatments = parse_input(args.input)
 
     name_a, df_a = treatments[0]
     name_b, df_b = treatments[1]
     name_ab, df_ab = treatments[2]
 
+    if args.adj_respA:
+        df_a = adjust_response(df_a, args.adj_respA[0], args.adj_respA[1])
+    if args.adj_respB:
+        df_b = adjust_response(df_b, args.adj_respB[0], args.adj_respB[1])
+
     # interpolate
     f_a = interpolate(df_a)
     f_b = interpolate(df_b)
 
+    # predict
     timepoints = np.arange(
         0, int(min(df_a['Time'].max(), df_b['Time'].max())), 0.01)
 
@@ -74,7 +110,7 @@ def main():
                               "Survival": combined_prob(f_a, f_b, timepoints,
                                                         rho=(args.min_rho + args.max_rho) / 2)})
 
-    # draw survival curves
+    # plot survival curve
     fig, ax = plt.subplots(figsize=(args.fig_width, args.fig_height))
     sns.despine()
     sns.lineplot(x='Time', y='Survival', data=df_a,
@@ -93,6 +129,7 @@ def main():
     ax.set_ylim(0)
     ax.set_xlabel("Time (months)")
     ax.set_ylabel('Survival (%)')
+
     # save output figure
     if args.out_prefix is None:
         fig.savefig('./{0}_{1}_combination_kmplot.pdf'.format(name_a, name_b))
